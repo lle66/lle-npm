58a7a9ee7669b243f5f975c3a58cfe2355698224
zhoule <2416305049@qq.com>
2021-09-08 10:55:08 +0800

    first init

---
+++ index.js
@@ -0,0 +1,11 @@
+/*
+ * @Author: zhoule
+ * @Date: 2021-09-07 16:59:59
+ * @Description: 入口文件
+ */
+exports.showMsg = function (arr) {
+  const result =[...new Set(arr.flat(Infinity))]
+  console.log("This is my first module",result)
+  //展开去重
+  return result
+};
